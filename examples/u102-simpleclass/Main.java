1d4d3e9b4b3881f39ca0e0ffe0c8c8c6e7d45d60
Rajasekaran S <proffraja@gmail.com>
2021-08-17 14:30:09 +0530

    a1

--- examples/u102-simpleclass/Main.java
+++ examples/u102-simpleclass/Main.java
@@ -1,5 +1,65 @@
-public class Main {
-    public static void main(String[] args) {
-        
+class Vehicle{
+    
+    String model;
+    String ownerName;
+    int maxSpeed;
+    boolean engineOn = false;
+    int currentSpeed = 0;
+    
+
+    Vehicle(String model,String ownerName,int maxSpeed){
+        this.model = model;
+        this.ownerName = ownerName;
+        this.maxSpeed = maxSpeed;
     }
+
+    void start(){
+        engineOn = true;
+        System.out.println("Engine ON");
+    }
+
+    void accelerate(){
+        currentSpeed = currentSpeed + 10;
+        System.out.println("Vehicle Accelerated to "+currentSpeed+" km/hr");
+    }
+
+    void breaking(){
+        currentSpeed = currentSpeed - 5;
+        System.out.println("Vehicle Deaccelerated to "+currentSpeed+" km/hr");
+    }
+
+    void stop(){
+        currentSpeed = 0;
+        engineOn = false;
+        System.out.println("Engine OFF");
+    }
+
+    void displayInfo(){
+        System.out.println("\n\nMODEL:"+model);
+        System.out.println("OWNER:"+ownerName);
+        System.out.println("MAX SPEED:"+maxSpeed);
+    }
+
+    void getStatus(){
+        if(engineOn){
+            System.out.println("ENGINE ON");
+        }
+        else{
+            System.out.println("ENGINE OFF");
+        }
+        System.out.println("Current Speed: "+currentSpeed+" km/hr");
+    }
+
+}
+
+class Main{
+    
+    public static void main(String[] args){
+        Vehicle v1 = new Vehicle("Swift", "Rajasekaran", 240);
+        v1.displayInfo();
+
+        Vehicle v2 = new Vehicle("Wangon R", "Sathish R", 180);
+        v2.displayInfo();
+    }
+
 }
---
+++ homeworks/a1/README.md
@@ -0,0 +1,7 @@
+# create a simple class that mimics "Dog"
+
+1. It has a name
+2. It has a owner
+3. It sleeps,eats,barks
+4. It feels happy (when owner at home) sad (when     owner not in home).
+5. It barks for an unknown person if he/she enters into proximity.
