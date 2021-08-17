76805f2ff98d50dcfc5ef1281c04d3c27b7d4d56
Rajasekaran S <proffraja@gmail.com>
2021-08-17 14:27:40 +0530

    cons

---
+++ examples/u103-constructor-types/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Student{
+    String name;
+    
+
+    Student(String name){
+        this.name = name;
+    }
+    
+}
+class Main {
+    public static void main(String[] args) {
+        Student studentOne = new Student("Rajasekaran S");
+        System.out.println("My name is "+s1.name);
+
+    }
+}
