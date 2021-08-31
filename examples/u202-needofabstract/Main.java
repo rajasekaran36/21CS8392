b9cbc45355f24317fea335bb49fb5c69b89ca66c
Rajasekaran S <proffraja@gmail.com>
2021-08-31 11:22:31 +0530

    abstarct

--- examples/u202-needofabstract/Main.java
+++ examples/u202-needofabstract/Main.java
@@ -1,25 +1,39 @@
-class Vehicle {
-
+abstract class Vehicle {
+    abstract public void start();
 }
 
-class Cycle {
-
+class Cycle extends Vehicle {
+    public void start() {
+        System.out.println("Unlock your Cycle");
+    }
 }
 
-class MotorVehicle {
-
+abstract class MotorVehicle extends Vehicle {
+    public void stop() {
+        System.out.println("Twist Key Anticlockwise");
+    }
 }
 
-class Bike {
-
+class Bike extends MotorVehicle {
+    public void start() {
+        System.out.println("Kick Start");
+    }
 }
 
-class Car {
-
+class Car extends MotorVehicle {
+    public void start() {
+        System.out.println("Twist Key");
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am main");
+        Car car = new Car();
+        Bike bike = new Bike();
+        Cycle cycle = new Cycle();
+
+        car.start();
+        bike.start();
+        cycle.start();
     }
 }
