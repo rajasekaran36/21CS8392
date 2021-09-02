662303b86e71b0548721d7c2566acd1c7601dcaa
KITE FACULTY <kitefaculty@kgisledu.com>
2021-09-02 10:28:20 +0530

    bike unloack

---
+++ examples/u206-unlock-bike/Main.java
@@ -0,0 +1,83 @@
+class Key{
+    private int code;
+    public Key(int code){
+        this.code = code;
+    }
+    public int getCode(){
+        return code;
+    }
+}
+class Lock{
+    int keyCode = 12134;
+    private boolean locked = true;
+    private Key key;
+    public Lock(){
+        key = new Key(this.keyCode);
+    }
+    public void doLock(){
+        this.locked = true;
+    }
+    public void doUnlock(Key key){
+        if(key.getCode() == keyCode)
+            this.locked = false;
+    }
+    public boolean isLocked(){
+        if(locked == true){
+            return true;
+        }
+        return false;
+    }
+    public Key getKey(){
+        return key;
+    }
+}
+
+abstract class Vehicle{
+    private Lock lock;
+    public Vehicle(){
+        lock = new Lock();    
+    }
+    abstract public void start();
+    public void unLock(Key key){
+        lock.doUnlock(key);
+    }
+    public boolean isLockOpen(){
+        if(!lock.isLocked())
+            return true;
+        else
+            return false;
+    }
+    public Key getKey(){
+        return lock.getKey();
+    }
+}
+
+class Bike extends Vehicle{
+    @Override
+    public void start(){
+        if(isLockOpen()){
+            System.out.println("Please Do Kick Start");
+        }
+        else{
+            System.out.println("Please unlock your vehicle");
+        }
+    }
+}
+
+class Owner{
+    private Bike bike;
+    private Key bikeKey;
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+       //Reg
+       Bike bike = new Bike();
+       Key key = bike.getKey();
+
+       //Road
+       bike.unLock(key);
+       bike.start();
+    }
+}
